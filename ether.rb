d59ad5674c9326a847c76aa93ffab57d4dfc0c2b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-26 13:22:08 -0500

    Removed test block

--- ether.rb
+++ ether.rb
@@ -11,17 +11,4 @@ class Ether < Formula
     system "mkdir ~/Library/Application\ Support/Ether"
     system "mkdir ~/Library/Application\ Support/Ether/Templates"
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Ether`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
