b7c0a992ad10555e09180bdd1f9cda4c33a846b0
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-15 09:06:30 -0600

    Updated version number to 1.9.0

--- ether.rb
+++ ether.rb
@@ -1,6 +1,6 @@
 class Ether < Formula
   homepage "https://github.com/calebkleveter/Ether"
-  version "1.8.0"
+  version "1.9.0"
   url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
   sha256 "4bbf64cf622987375ffa4d43aafe24dc47c393aef362f8984931f4bb0131a734"
 
