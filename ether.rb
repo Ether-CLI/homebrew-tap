11fee26d474aaf2483b94fd427b608c05b6cd181
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-27 13:10:35 -0600

    Updated version number and hash for version 1.9.2

--- ether.rb
+++ ether.rb
@@ -1,8 +1,8 @@
 class Ether < Formula
   homepage "https://github.com/calebkleveter/Ether"
-  version "1.9.1"
+  version "1.9.2"
   url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
-  sha256 "57458a8a1b1eea100fc6f887b82751ad1789b453b063b1e71fe4c6e9941b30cf"
+  sha256 "a276d2e20defb6cb275b05252715804b711e52f0d7590d72b883e8dd56d3dd18"
 
   depends_on "ctls" => :run
 
