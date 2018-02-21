c388610aa9eb07b38a5a9baa705bdd8f95507e70
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-21 16:26:37 -0600

    Brew mkdir test

--- ether.rb
+++ ether.rb
@@ -1,6 +1,6 @@
 class Ether < Formula
   homepage "https://github.com/calebkleveter/Ether"
-  version "1.10.0"
+  version "1.10.0.1"
   url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
   sha256 "073149de0f91b4edb2eaad8d9b76c0823ac986310ceafdab0d136d69e861dabf"
 
@@ -8,6 +8,6 @@ class Ether < Formula
 
   def install
     bin.install "ether"
-    system "mkdir", "-p", "$HOME/Library/Application Support/Ether/Templates"
+    system "mkdir", "-p", "$HOME/Library/Application\\ Support/Ether/Templates"
   end
 end
