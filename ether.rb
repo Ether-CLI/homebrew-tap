bd3cf2d03a4b7304765e65fb02abf79c10a9f5ab
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-21 16:47:47 -0600

    Set version back to production

--- ether.rb
+++ ether.rb
@@ -1,7 +1,7 @@
 class Ether < Formula
   homepage "https://github.com/calebkleveter/Ether"
-  version "1.10.0.2"
-  url "https://github.com/calebkleveter/Ether/releases/download/1.10.0/macOS-sierra.tar.gz"
+  version "1.10.0"
+  url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
   sha256 "073149de0f91b4edb2eaad8d9b76c0823ac986310ceafdab0d136d69e861dabf"
 
   depends_on "ctls" => :run
