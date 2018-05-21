48d83311cc9b0fe6d4c79ccc9fe4301d8d69ab57
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-21 05:33:20 -0500

    Removed deprecated depends_on call

--- ether.rb
+++ ether.rb
@@ -4,8 +4,6 @@ class Ether < Formula
   url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
   sha256 "e29059c3fc98c930e30a868acaf0dada99b7b209610a9a61efdca2a2d6de0ceb"
 
-  depends_on "ctls" => :run
-
   def install
     bin.install "ether"
     ohai "Run `mkdir -p $HOME/Library/Application\\ Support/Ether/Templates` to complete installation"
