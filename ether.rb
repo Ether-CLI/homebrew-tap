7512ff045bc9d949f5382cccbd7423a0132a96e6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-25 12:14:37 -0500

    Updated version to 2018.05.25

--- ether.rb
+++ ether.rb
@@ -1,8 +1,8 @@
 class Ether < Formula
   homepage "https://github.com/Ether-CLI/Ether"
-  version "2018.05.22"
+  version "2018.05.25"
   url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
-  sha256 "35713694b3d9311626951305c2c9dd1a57c2f0b2361f9cfefc0f2164c26bce68"
+  sha256 "587d27c5e2846f467c488b806f073b0821856fcf525a916e8cebd62cb7adfe43"
 
   def install
     bin.install "ether"
