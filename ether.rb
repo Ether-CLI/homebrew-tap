5460ed1c0c60279e194ff0d3fe93b552a8b7f699
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-08 15:32:53 -0500

    Fake version number to force upgrade

--- ether.rb
+++ ether.rb
@@ -1,7 +1,7 @@
 class Ether < Formula
   homepage "https://github.com/Ether-CLI/Ether"
-  version "2018.05.25"
-  url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
+  version "2018.05.25.1"
+  url "https://github.com/calebkleveter/Ether/releases/download/2018.05.25/macOS-sierra.tar.gz"
   sha256 "587d27c5e2846f467c488b806f073b0821856fcf525a916e8cebd62cb7adfe43"
 
   depends_on "libressl"
