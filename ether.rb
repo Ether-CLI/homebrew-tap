8922775460c31b80a6e2f9837f00a843e720bf58
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-08 15:31:45 -0500

    Added lbressl dependency

--- ether.rb
+++ ether.rb
@@ -4,6 +4,8 @@ class Ether < Formula
   url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
   sha256 "587d27c5e2846f467c488b806f073b0821856fcf525a916e8cebd62cb7adfe43"
 
+  depends_on "libressl"
+  
   def install
     bin.install "ether"
   end
