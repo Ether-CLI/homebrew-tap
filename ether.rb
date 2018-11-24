3f8c0f05407a183fc14d09cea886813f2404a4b2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-24 11:12:58 -0600

    Updated checksum for recompiled executable

--- ether.rb
+++ ether.rb
@@ -2,7 +2,7 @@ class Ether < Formula
   homepage "https://github.com/Ether-CLI/Ether"
   version "2018.10.03"
   url "https://github.com/calebkleveter/Ether/releases/download/#{version}/macOS-sierra.tar.gz"
-  sha256 "9e9b148a206ca63d26a515b963c9eebe492fffc6dadaeab329d09452e2488f5a"
+  sha256 "5d8d8b50bfac4adfd3a4b68abb4d367f142b5dba77bac96e72321b4a75de0618"
 
   depends_on "libressl"
 
